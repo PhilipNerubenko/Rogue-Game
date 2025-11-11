8eb77f08834cc344eb0589acb45c11b83387a530
Hardy31 <alex-vav@list.ru>
2025-11-11 11:55:57 +0300

    fix JCurses

--- src/Rogue1980/build.gradle.kts
+++ src/Rogue1980/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -8,17 +9,27 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     flatDir {
-        dirs("libs")  // Без точки с запятой, в скобках
+        dirs("libs")
     }
 }
 
 dependencies {
-    // https://jarcasting.com/artifacts/com.baulsupp.kolja/jcurses/0.9.5.3/
-    implementation(files("libs/jcurses-0.9.5.3.jar"))  // файл jcurses.jar в папке libs/
+    implementation(files("libs/jcurses-0.9.5.3.jar"))
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+// Укажите главный класс с main() вашей программы
+application {
+    mainClass.set("org.example.Main")
+    applicationDefaultJvmArgs = listOf("-Djava.library.path=${projectDir}/libs")
+}
+
+// Обеспечим, что JVM параметр java.library.path передается в другие задачи запуска
+tasks.withType<JavaExec> {
+    jvmArgs = listOf("-Djava.library.path=${projectDir}/libs")
 }
 
 tasks.test {
--- src/Rogue1980/src/main/java/org/example/Main.java
+++ src/Rogue1980/src/main/java/org/example/Main.java
@@ -1,21 +1,29 @@
 package org.example;
 
+import jcurses.system.CharColor;
 import jcurses.system.Toolkit;
 import jcurses.widgets.*;
 
 public class Main {
     public static void main(String[] args) {
+//        Toolkit.init();
+//        Window window = new Window(5, 5, 30, 10, true, "Привет");
+//        Toolkit.printString("Hello, JCurses!", 2, 2, new CharColor(CharColor.WHITE, CharColor.BLUE));
+//        window.show();
+//        Toolkit.readCharacter();
+//        window.close();
+//        Toolkit.shutdown();
+
+
         Toolkit.init();
-
-        Window window = new Window(5, 5, 30, 10, true, "Привет");
-
-
-        Label label = new Label("Hello, JCurses!");
-        window.add(label);
+        Window window = new Window(10, 10, 80, 80, false, "Example");
+        Toolkit.printString("Hello, JCurses1!", 5, 5, new CharColor(CharColor.BLACK,
+                CharColor.RED));
 
         window.show();
-        Toolkit.readCharacter();
-        window.close();
+        Toolkit.printString("Hello, JCurses2!", 5, 7, new CharColor(CharColor.BLUE, CharColor.WHITE));
         Toolkit.shutdown();
     }
+
+
 }
